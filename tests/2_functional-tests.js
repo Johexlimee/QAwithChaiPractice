c826454b201be0c5f4c27acf88c4ffbf0b0d6b82
Johexlimee <Jhk0810p@gmail.com>
2025-04-28 10:49:29 -0500

    Integration Test with Chai-HTTP

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -28,8 +28,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
@@ -39,32 +39,57 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200, 'Status should be 200');
+          assert.equal(res.type, 'application/json', 'Content-Type should be application/json');
+          assert.equal(res.body.name, 
+            'Cristoforo',
+            'Name should be Cristoforo'
+          );
+          assert.equal(res.body.surname, 
+            'Colombo', 
+            'Surname should be Colombo'
+          );
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'Status should be 200');
+          assert.equal(res.type, 'application/json', 'Content-Type should be application/json');
+          assert.equal(res.body.name, 
+            'Giovanni',
+            'Name should be Giovanni'
+          );
+          assert.equal(res.body.surname, 
+            'da Verrazzano', 
+            'Surname should be da Verrazzano'
+          );
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
-
+Browser.site = 'http://localhost:3000/'; // Your URL here
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
-
-
+  const browser = new Browser();
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+      assert.equal(browser.site);
     });
   });
 
